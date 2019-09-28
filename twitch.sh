622696a1183279d558f3f3e0f970e3e7bc7aae74
zizzu <39168861+zizzu0@users.noreply.github.com>
2019-09-28 23:57:41 +0200

    Update twitch.sh

--- twitch.sh
+++ twitch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# watch a twitch stream via mpv and irssi
+# requires streamlink, mpv, irssi
+# usage: twitch.sh channelname
+# irssi does not automatically switch to the channel, type /window 2 to focus it.
 
 set -euo pipefail
 
@@ -9,7 +13,7 @@ IRSSICONF="servers = (
     address = 'irc.twitch.tv';
     chatnet = 'Twitch';
     port = '6697';
-    password = 'oauth:46sh4wje1xxvkejs1z9peqrs5ld6r4';
+    password = 'oauth:youroauthhere';
     use_ssl = 'yes';
     ssl_verify = 'no';
     autoconnect = 'yes';
@@ -29,7 +33,7 @@ channels = (
 
 settings = {
   core = {
-    nick = 'whimsicaltraveler2463';
+    nick = 'yournicknamehere';
   }
 }
 
