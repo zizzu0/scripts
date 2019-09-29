ed1ddad9707d69d5b15f43fe0400401be2d8993a
zizzu <39168861+zizzu0@users.noreply.github.com>
2019-09-29 11:58:58 +0200

    Update twitch.sh

--- twitch.sh
+++ twitch.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 # watch a twitch stream via mpv and irssi
-# requires streamlink, mpv, irssi
+# requires streamlink, mpv, irssi, a client_id and an oauth
 # usage: twitch.sh channelname
 # irssi does not automatically switch to the channel, type /window 2 to focus it.
 
-set -euo pipefail
+if [ -z "$1" ];then echo "Usage $0 channelname"; exit 1; fi
 
 channel="$1"
+
+# python script to check if the streamer is live via twitch api
+# the client_id is per app, this one i found on google via
+# twitch client id site:pastebin.com
+STATUS="#!/usr/bin/env python3
+
+import json
+import requests
+
+client_id = 'whnghno3rybgm9wp6bl97vwu43d2qo'
+link = 'https://api.twitch.tv/helix/streams?user_login=' + '$1'
+accept = 'application/vnd.twitchtv.v5+json'
+
+r = requests.get(link, headers={'Client-ID':client_id, 'Accept': accept})
+
+resp_dict = json.loads(r.text)
+
+for i in resp_dict:
+    if i == 'data' and len(resp_dict[i]) > 0:
+        print(resp_dict['data'][0]['type'])"
 
 IRSSICONF="servers = (
   {
     address = 'irc.twitch.tv';
     chatnet = 'Twitch';
     port = '6697';
-    password = 'oauth:youroauthhere';
+    password = 'oauth:46sh4wje1xxvkejs1z9peqrs5ld6r4';
     use_ssl = 'yes';
     ssl_verify = 'no';
     autoconnect = 'yes';
@@ -33,7 +53,7 @@ channels = (
 
 settings = {
   core = {
-    nick = 'yournicknamehere';
+    nick = 'whimsicaltraveler2463';
   }
 }
 
@@ -43,6 +63,10 @@ ignores = {
   }
 }"
 
+
+status=$(python3 <(echo "$STATUS"))
+
+if [ -z $status ];then echo "Error: user is not streaming right now"; exit 1; fi
 
 streamlink --player mpv https://www.twitch.tv/$1 best >> /dev/null 2>&1 &
 irssi --config <(echo "$IRSSICONF")
